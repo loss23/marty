3263117c305d0f9c12f82ba9ac7b2b96d6acd8c3
loss23 <61843498+loss23@users.noreply.github.com>
2025-08-15 00:02:01 -0500

    Update mercury.lua

--- mercury.lua
+++ mercury.lua
@@ -72,9 +72,9 @@ local Library = {
 			WeakText = Color3.fromHSV(0, 0, 172/255)
 		},
 		PH = {
-			Main = Color3.fromRGB(255, 115, 0),
-			Secondary = Color3.fromRGB(255, 115, 0),
-			Tertiary = Color3.fromRGB(255, 115, 0),
+			Main = Color3.fromRGB(0,0,0),
+			Secondary = Color3.fromRGB(9,9,9),
+			Tertiary = Color3.fromRGB(255,255,255),
 
 			StrongText = Color3.fromHSV(255, 115, 0),		
 			WeakText = Color3.fromHSV(255, 140, 45)
